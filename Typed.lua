84c166bdd95ef5030b02c4c79b8d772ffbc78009
Miqueas Martinez <miqueas2020@yahoo.com>
2021-02-24 18:35:47 -0500

    "Structs" y funciones con tipo de retorno

--- Typed.lua
+++ Typed.lua
@@ -1,41 +1,200 @@
-local SetMT = setmetatable
-local GetMT = getmetatable
+local SetMT  = setmetatable
+local GetMT  = getmetatable
+local Unpack = table.unpack or unpack
 
 --[[ Spec:
 
-[DONE] (type)     @Type   ==> just @Type
-[IN PROCCESS] (struct)    @Type{} ==> a table of @Type
-[IN PROCCESS] (return)   @Type() ==> a function that returns @Type
-[DONE] (multiple) { ... } ==> can be multiple types
+  [DONE]    (Single)   @Type   ==> just @Type
+  [WORKING] (Struct)   @Type{} ==> a table of @Type
+  [WORKING] (Return)   @Type() ==> a function that returns @Type
+  [PENDING] (Multiple) { ... } ==> can be multiple types
 
 ]]
 
-local Typed = {
-  -- Registry
-  _Reg = {},
-  -- Types
-  _Types = {
-    ["@Str"]    = "string",
-    ["@Num"]    = "number",
-    ["@Bool"]   = "boolean",
-    ["@User"]   = "userdata",
-    ["@Func"]   = "function",
-    ["@Table"]  = "table",
-    ["@Thread"] = "thread"
-  }
+--[[ Lua data types
+
+  This table contains the equivalent Typed types of
+  supported Lua types by Typed
+
+]]
+local _LTypes = {
+  ["string"]   = "@Str",
+  ["number"]   = "@Num",
+  ["boolean"]  = "@Bool",
+  ["userdata"] = "@User",
+  ["function"] = "@Func",
+  ["table"]    = "@Table",
+  ["thread"]   = "@Thread"
 }
 
-function Typed:new(TDecl, Key, ...)
+--[[ Typed data types
+
+  Same as the above table, but reversed.
+
+]]
+local _TTypes = {
+  -- Single types
+  ["@Str"]    = "string",
+  ["@Num"]    = "number",
+  ["@Bool"]   = "boolean",
+  ["@User"]   = "userdata",
+  ["@Func"]   = "function",
+  ["@Table"]  = "table",
+  ["@Thread"] = "thread",
+
+  -- Struct types
+  ["@Str{}"]    = "string",
+  ["@Num{}"]    = "number",
+  ["@Bool{}"]   = "boolean",
+  ["@User{}"]   = "userdata",
+  ["@Func{}"]   = "function",
+  ["@Table{}"]  = "table",
+  ["@Thread{}"] = "thread",
+
+  -- Return types
+  ["@Str()"]    = "string",
+  ["@Num()"]    = "number",
+  ["@Bool()"]   = "boolean",
+  ["@User()"]   = "userdata",
+  ["@Func()"]   = "function",
+  ["@Table()"]  = "table",
+  ["@Thread()"] = "thread"
+}
+
+--[[ Typed registry
+
+  A registry were Typed store all created values
+
+]]
+local _TReg = {}
+
+-- The Typed class
+local Typed = {}
+
+-- Returns the type declaration mode
+local function Parse(Dec)
+  local Single = "^ *@%a+ *$"
+  local Struct = "^ *@%a+ *%{ *%} *$"
+  local Return = "^ *@%a+ *%( *%) *$"
+
+  local dt, s, t = type(Dec), "string", "table"
+
+  if dt == s then
+    if Dec:match(Single) then
+      return "Single"
+    elseif Dec:match(Struct) then
+      return "Struct"
+    elseif Dec:match(Return) then
+      return "Return"
+    else
+      return nil
+    end
+  elseif dt == t then
+    return "Multiple"
+  else
+    return nil
+  end
+end
+
+-- Constructs a table for a Single type value
+local function Build_Single(T, V)
+  local ErrMsg = "Typed:new [ERROR] => Type mismatch, %s (%s) expected, got %s (%s)"
+  local VT = type(V)
+
+  assert(
+    VT == _TTypes[T],
+    ErrMsg:format(T, _TTypes[T], _LTypes[VT], VT)
+  )
+
+  return {
+    _type = _TTypes[T],
+    _val = V,
+    _is = "Single"
+  }
+end
+
+local function Build_Struct(T, ...)
+  local va = { ... }
+  local struct = {
+    _type = _TTypes[T],
+    _is = "Struct"
+  }
+
+  if #va == 1 then
+    for k, v in pairs(va[1]) do
+      if type(v) ~= _TTypes[T] then
+        return error("Key/index '" .. k .. "' don't match the type " .. T)
+      end
+    end
+
+    struct._val = va[1]
+  elseif #va > 1 then
+    struct._val = {}
+
+    for i, v in ipairs(va) do
+      if type(v) ~= _TTypes[T] then
+        return error("Key/index '" .. i .. "' don't match the type " .. T)
+      else
+        table.insert(struct._val, v)
+      end
+    end
+  end
+
+  return struct
+end
+
+local function Build_Return(T, F, ...)
+  local ret = {
+    _type = _TTypes[T],
+    _is = "Return"
+  }
+
+  assert(type(T) == "string", "Bad argument #1, string expected, got " .. type(T))
+  assert(type(F) == "function", "Bad argument #2, function expected, got " .. type(F))
+
+  local Ok, Val = pcall(F, ...)
+
+  if not Ok then
+    return error("Something's went wrong calling the function!")
+  elseif Ok and type(Val) ~= _TTypes[T] then
+    return error("Type returned don't match type " .. T)
+  elseif Ok and type(Val) == _TTypes[T] then
+    ret._val = F
+    return ret
+  end
+end
+
+-- local function Build_Multiple(Types, ...)
+--   local match = false
+--   local mult = {
+--     _type = Types,
+--     _is = "Multiple"
+--   }
+
+--   for i, v in ipairs(Types) do
+--     if type(va[1]) == self._Types[v] then
+--       match = true
+--     end
+--   end
+
+--   if match then
+--     self._Reg[Key]._val = va[1]
+--   else
+--     return error("Typed:new [ERROR] => Value don't match with any of specified types")
+--   end
+-- end
+
+function Typed:new(Dec, Key, ...)
   local va = { ... }
 
   -- Some type checking
   assert(
-    type(TDecl) == "string" or type(TDecl) == "table",
-    "Bad argument #1 for Typed:new, table or string expected, got " .. type(TDecl)
+    type(Dec) == "string" or type(Dec) == "table",
+    "Bad argument #1 for Typed:new, table or string expected, got " .. type(Dec)
   )
 
   assert(
-    #TDecl > 0,
+    #Dec > 0,
     "Argument #1 is given, but length is 0 (in Typed:new)"
   )
 
@@ -44,97 +203,24 @@ function Typed:new(TDecl, Key, ...)
     "Bad argument #2 for Typed:new, string expected, got " .. type(Key)
   )
 
-  assert(not self._Reg[Key], "Key '" .. Key .. "' already's exists (in Typed:new)")
+  assert(not _TReg[Key], "Key '" .. Key .. "' already's exists (in Typed:new)")
   -- End type checking
 
-  if type(TDecl) == "string" then
-    if not self:parse(TDecl) then
-      return error("Typed:new [ERROR] => Bad type declaration: " .. TDecl)
-
-    elseif self:parse(TDecl) == "type" then
-      local ErrMsg = "Typed:new [ERROR] => Type mismatch, %s expected, got %s"
-
-      assert(
-        type(va[1]) == self._Types[TDecl],
-        ErrMsg:format(self._Types[TDecl], type(va[1]))
-      )
-
-      self._Reg[Key] = {
-        _type = self._Types[TDecl],
-        _val = va[1],
-        _is = "type"
-      }
-
-    elseif self:parse(TDecl) == "struct" then
-      local tstr = TDecl:gsub("[%{%}]", "")
-
-      self._Reg[Key] = {
-        _type = self._Types[tstr],
-        _is = "struct"
-      }
-
-      if #va == 1 and type(va[1]) == "table" then
-        for k, v in pairs(va[1]) do
-          if type(v) ~= self._Types[tstr] then
-            return error("Key/index '" .. k .. "' don't match the type " .. tstr)
-          end
-        end
-
-        self._Reg[Key]._val = va[1]
-      else
-        self._Reg[Key]._val = {}
-
-        for i, v in ipairs(va) do
-          if type(v) ~= self._Types[tstr] then
-            return error("Key/index '" .. i .. "' don't match the type " .. tstr)
-          else
-            table.insert(self._Reg[Key]._val, v)
-          end
-        end
-      end
-
-    elseif self:parse(TDecl) == "return" then
-      local tstr = TDecl:gsub("[%(%)]", "")
-
-      self._Reg[Key] = {
-        _type = self._Types[tstr],
-        _is = "return"
-      }
-
-      if type(va[1]) == "function" and type(va[1]()) == self._Types[tstr] then
-        self._Reg[Key]._val = va[1]
-      elseif type(va[1]) == "function" and type(va[1]()) ~= self._Types[tstr] then
-        return error("Return value don't match the type " .. tstr)
-      else
-        return error("Value isn't a function")
-      end
-    end
-
-  elseif type(TDecl) == "table" then
-    local match = false
-
-    self._Reg[Key] = {
-      _type = TDecl,
-      _is = "multiple"
-    }
-
-    for i, v in ipairs(TDecl) do
-      if type(va[1]) == self._Types[v] then
-        match = true
-      end
-    end
-
-    if match then
-      self._Reg[Key]._val = va[1]
-    else
-      return error("Typed:new [ERROR] => Value don't match with any of specified types")
-    end
+  if Parse(Dec) == "Single" then
+    _TReg[Key] = Build_Single(Dec, va[1])
+  elseif Parse(Dec) == "Struct" then
+    _TReg[Key] = Build_Struct(Dec, ...)
+  elseif Parse(Dec) == "Return" then
+    local fn   = table.remove(va, 1)
+    _TReg[Key] = Build_Return(Dec, fn, Unpack(va))
+  else
+    return error("Typed:new [ERROR] => Bad type declaration: " .. Dec)
   end
 end
 
 function Typed:get(Key)
-  if self._Reg[Key] then
-    return self._Reg[Key]._val
+  if _TReg[Key] then
+    return _TReg[Key]._val
   else
     return error("Key '" .. Key .. "' doesn't exists")
   end
@@ -143,45 +229,18 @@ end
 function Typed:set(Key, ...)
   local va = { ... }
 
-  if self._Reg[Key] then
+  if _TReg[Key] then
   else
     return error("Key '" .. Key .. "' doesn't exists")
   end
 end
 
-function Typed:parse(TDecl)
-  local TName   = "^ *@%a+ *$"
-  local TTable  = "^ *@%a+ *%{ *%} *$"
-  local TReturn = "^ *@%a+ *%( *%) *$"
-
-  if TDecl:match(TName) then
-    return "type"
-  elseif TDecl:match(TTable) then
-    return "struct"
-  elseif TDecl:match(TReturn) then
-    return "return"
-  else
-    return nil
-  end
-end
-
 Typed = SetMT(Typed, {
-  __call = Typed.new,
-  __index = Typed.get,
+  __call     = Typed.new,
+  __index    = Typed.get,
   __newindex = Typed.set
 })
 
--- Nice
-Typed("@Str()", "fn1", function ()
-  return "Yes"
-end)
-
-print(Typed.fn1)
-
--- Error
-Typed("@Num()", "fn2", function ()
-  return "No"
-end)
-
--- Never reached
-print(Typed.fn2)
+Typed('@Str', "name", "Jhon")
+Typed('@Num', "age", 35)
+print(Typed.name .. " is " .. Typed.age)
